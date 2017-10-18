40c41f5cdc6a4dca8d98e066376b54d294a322b0
Brian Donnoe <dunow33@hotmail.com>
2017-10-18 13:12:54 -0400

    fixes

--- my-app/src/routes.ts
+++ my-app/src/routes.ts
@@ -2,12 +2,12 @@ import {Routes} from '@angular/router';
 import {SignupFormComponent} from './app/signup-form/signup-form.component';
 import {LoginFormComponent} from './app/login-form/login-form.component';
 import {ChatroomComponent} from './app/chatroom/chatroom.component';
-import {MessageInputComponent} from './app/feedback/message-input.component';
+//import {MessageInputComponent} from './app/feedback/message-input.component';
 
 export const appRoutes: Routes = [
 	{path: 'signup', component: SignupFormComponent},
 	{path: 'login', component: LoginFormComponent},
 	{path: 'chat', component: ChatroomComponent},
-	{path: 'feedback', component: MessageInputComponent},
+	//{path: 'feedback', component: MessageInputComponent},
 	{path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
