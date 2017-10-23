ae7b5cf5043835b3515f45c056706c00f2a64d3c
Jordan <jordanalex999@hotmail.com>
2017-10-23 15:39:21 -0400

    added display route

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AuthService } from './services/auth.service';
 
 import { appRoutes } from '../routes';
 import { environment } from '../environments/environment';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { environment } from '../environments/environment';
     SignupFormComponent,
     NavbarComponent,
     UserListComponent,
-    UserItemComponent
+    UserItemComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
