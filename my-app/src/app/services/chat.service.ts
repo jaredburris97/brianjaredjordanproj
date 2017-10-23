e5280f885eaad2165c76ddc7fbacd1ea91d1b6eb
Jared Burris <jaredburris97@gmail.com>
2017-10-23 14:23:41 -0400

    Merge failed deleting devtest

--- my-app/src/app/services/chat.service.ts
+++ my-app/src/app/services/chat.service.ts
@@ -9,67 +9,67 @@ import { ChatMessage } from '../models/chat-message.model';
 
 @Injectable()
 export class ChatService {
-  user: firebase.User;
-  chatMessages: FirebaseListObservable<ChatMessage[]>;
-  chatMessage: ChatMessage;
-  userName: Observable<string>;
+ user: firebase.User;
+ chatMessages: FirebaseListObservable<ChatMessage[]>;
+ chatMessage: ChatMessage;
+ userName: Observable<string>;
 
-  constructor(
-    private db: AngularFireDatabase,
-    private afAuth: AngularFireAuth
-    ) {
-        this.afAuth.authState.subscribe(auth => {
-          if (auth !== undefined && auth !== null) {
-            this.user = auth;
-          }
+ constructor(
+   private db: AngularFireDatabase,
+   private afAuth: AngularFireAuth
+   ) {
+       this.afAuth.authState.subscribe(auth => {
+         if (auth !== undefined && auth !== null) {
+           this.user = auth;
+         }
 
-          this.getUser().subscribe(a => {
-            this.userName = a.displayName;
-          });
-        });
-    }
+         this.getUser().subscribe(a => {
+           this.userName = a.displayName;
+         });
+       });
+   }
 
-  getUser() {
-    const userId = this.user.uid;
-    const path = `/users/${userId}`;
-    return this.db.object(path);
-  }
+ getUser() {
+   const userId = this.user.uid;
+   const path = `/users/${userId}`;
+   return this.db.object(path);
+ }
 
-  getUsers() {
-    const path = '/users';
-    return this.db.list(path);
-  }
+ getUsers() {
+   const path = '/users';
+   return this.db.list(path);
+ }
 
-  sendMessage(msg: string) {
-    const timestamp = this.getTimeStamp();
-    const email = this.user.email;
-    this.chatMessages = this.getMessages();
-    this.chatMessages.push({
-      message: msg,
-      timeSent: timestamp,
-      userName: this.userName,
-      email: email });
-  }
+ sendMessage(msg: string) {
+   const timestamp = this.getTimeStamp();
+   const email = this.user.email;
+   this.chatMessages = this.getMessages();
+   this.chatMessages.push({
+     message: msg,
+     timeSent: timestamp,
+     userName: this.userName,
+     email: email });
+ }
 
-  getMessages(): FirebaseListObservable<ChatMessage[]> {
-    // query to create our message feed binding
-    return this.db.list('messages', {
-      query: {
-        limitToLast: 25,
-        orderByKey: true
-      }
-    });
-  }
+ getMessages(): FirebaseListObservable<ChatMessage[]> {
+   // query to create our message feed binding
+   return this.db.list('messages', {
+     query: {
+       limitToLast: 25,
+       orderByKey: true
+     }
+   });
+ }
 
-  getTimeStamp() {
-    const now = new Date();
-    const date = now.getUTCFullYear() + '/' +
-                 (now.getUTCMonth() + 1) + '/' +
-                 now.getUTCDate();
-    const time = now.getUTCHours() + ':' +
-                 now.getUTCMinutes() + ':' +
-                 now.getUTCSeconds();
+ getTimeStamp() {
+   const now = new Date();
+   const date = now.getUTCFullYear() + '/' +
+                (now.getUTCMonth() + 1) + '/' +
+                now.getUTCDate();
+   const time = now.getUTCHours() + ':' +
+                now.getUTCMinutes() + ':' +
+                now.getUTCSeconds();
 
-    return (date + ' ' + time);
-  }
+   return (date + ' ' + time);
+ }
 }
