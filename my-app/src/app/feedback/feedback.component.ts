bce75a9d4a17b7d5c93b12671ee90b08ff99daa0
Brian Donnoe <dunow33@hotmail.com>
2017-10-22 22:45:23 -0400

    working on styling the feedback messages

--- my-app/src/app/feedback/feedback.component.ts
+++ my-app/src/app/feedback/feedback.component.ts
@@ -4,7 +4,23 @@ import { FeedbackService } from '../services/feedback.service';
 
 @Component({
   selector: 'app-feedback',
-  templateUrl: './feedback.component.html'
+  templateUrl: './feedback.component.html',
+  styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+        }
+
+        .config {
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+
+        }
+  `]
 })
 export class FeedbackComponent {
   @Input() feedback: Feedback;
