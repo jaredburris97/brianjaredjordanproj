e559af950101f60c6a6500a0d24875b106135c95
Jared Burris <jaredburris97@gmail.com>
2017-10-25 16:02:00 -0400

    IFRAME WORKS

--- my-app/src/app/display/display.component.ts
+++ my-app/src/app/display/display.component.ts
@@ -2,26 +2,41 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-display',
+
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.css']
+
+  styleUrls: ['./display.component.css'],
+
 })
+
 export class DisplayComponent implements OnInit {
 
-  constructor() { }
+	private newLink;
+	private site;
 
-  ngOnInit() {
+	addLink(newLink: string) {
+		this.site = newLink;
+		if (newLink) {
+			document.getElementById("theframe").setAttribute('src', this.site);
+		}
+	}
+
+  constructor() {
+
   }
 
+  ngOnInit() {
+	
+  }
 }
 
-window.onload = function(){
-var link = "http://localhost:4200/chat"
-var iframe = document.createElement('iframe');
-iframe.frameBorder="0px";
-iframe.width="100%";
-iframe.height="50%";
-iframe.id="randomid";
-iframe.setAttribute("src", link);
-document.getElementById("ad54").appendChild(iframe);
-
-}
+// window.onload = function() {
+// 	var chatlink = "http://localhost:4200/chat"
+// 	var chatiframe = document.createElement('iframe');
+// 	chatiframe.frameBorder="0px";
+// 	chatiframe.width="50%";
+// 	chatiframe.height="100px";
+// 	chatiframe.id="randomid";
+// 	chatiframe.setAttribute("src", chatlink);
+// 	document.getElementById("chatFrame").appendChild(chatiframe);
+// }
